8af77320e19da9a77bd5a0394e28e93d9309f99d
Robin Malfait <malfait.robin@gmail.com>
2020-02-18 23:56:13 +0100

    inline one-time-used variable

--- src/pipe.ts
+++ src/pipe.ts
@@ -3,9 +3,8 @@ import { ensureFunction } from './utils/ensureFunction';
 type Fn = (...args: any) => any;
 
 export function pipe<T>(...fns: (Fn | Iterable<T>)[]): Fn {
-  const fn = fns.pop();
   return fns.reduceRight((f: Fn, g) => {
     const g_ = ensureFunction(g);
     return (...args) => f(g_(...args));
-  }, ensureFunction(fn));
+  }, ensureFunction(fns.pop()));
 }
