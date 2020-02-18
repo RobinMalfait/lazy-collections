fbcc779679af105628076d98feb2db0ed0c36efc
Robin Malfait <malfait.robin@gmail.com>
2020-02-18 23:44:58 +0100

    duplicate certain tests to verify that we don't mutate setup data

--- src/average.test.ts
+++ src/average.test.ts
@@ -5,4 +5,5 @@ it('should be possible to get an average of all the values', () => {
   const program = pipe(average());
 
   expect(program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(5);
+  expect(program([10, 10, 10])).toEqual(10);
 });
--- src/sum.test.ts
+++ src/sum.test.ts
@@ -6,10 +6,12 @@ it('should be possible to sum an array', () => {
   const program = pipe(sum());
 
   expect(program([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(55);
+  expect(program([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(55);
 });
 
 it('should be possible to sum an iterator', () => {
   const program = pipe(sum());
 
+  expect(program(range(0, 10))).toEqual(55);
   expect(program(range(0, 10))).toEqual(55);
 });
