1d133a4af6d8791ef7de1e05f5c55faf5da864e4
Robin Malfait <malfait.robin@gmail.com>
2020-03-23 21:44:30 +0100

    use plain tsc for builds

--- examples/001-alphabet.ts
+++
@@ -1,9 +0,0 @@
-import { pipe, map, range, toArray } from '..';
-
-const alphabet = pipe(
-  range(0, 25),
-  map((x: number) => String.fromCharCode(x + 65)),
-  toArray()
-);
-
-alphabet(); // [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
--- examples/002-random-numbers.ts
+++
@@ -1,6 +0,0 @@
-import { pipe, generate, take, toArray } from '..';
-
-const randomNumberGenerator = pipe(generate(Math.random), take(3), toArray());
-
-randomNumberGenerator();
-// [ 0.973565686837264, 0.7129324120429745, 0.10387686881346947 ]
