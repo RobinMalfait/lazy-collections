66cd9dbc81a71a2d54cb6e1e3a7b31b90432ee6b
Robin Malfait <malfait.robin@gmail.com>
2022-08-08 23:51:54 +0200

    add `flatMap` support

---
+++ src/flatMap.test.ts
@@ -0,0 +1,56 @@
+import { pipe } from './pipe'
+import { flatMap } from './flatMap'
+import { toArray } from './toArray'
+import { delay } from './delay'
+
+it('should be possible to flatMap data from A to B', () => {
+  let program = pipe(
+    flatMap((x: number) => [x * 1, x * 2, x * 4]),
+    toArray()
+  )
+
+  expect(program([1, 2, 3])).toEqual([1, 2, 4, 2, 4, 8, 3, 6, 12])
+  expect(program([1, 2, 3])).toEqual([1, 2, 4, 2, 4, 8, 3, 6, 12])
+})
+
+it('should return undefined when no stream is passing through it', () => {
+  let program = pipe(
+    flatMap((x: number) => [x * 1, x * 2, x * 4]),
+    toArray()
+  )
+
+  expect(program()).toEqual(undefined)
+  expect(program()).toEqual(undefined)
+})
+
+it('should be possible to flatMap data from A to B (async)', async () => {
+  let program = pipe(
+    delay(0),
+    flatMap((x: number) => [x * 1, x * 2, x * 4]),
+    toArray()
+  )
+
+  expect(await program([1, 2, 3])).toEqual([1, 2, 4, 2, 4, 8, 3, 6, 12])
+  expect(await program([1, 2, 3])).toEqual([1, 2, 4, 2, 4, 8, 3, 6, 12])
+})
+
+it('should be possible to flatMap data from A to B (Promise async)', async () => {
+  let program = pipe(
+    delay(0),
+    flatMap((x: number) => [x * 1, x * 2, x * 4]),
+    toArray()
+  )
+
+  expect(await program(Promise.resolve([1, 2, 3]))).toEqual([1, 2, 4, 2, 4, 8, 3, 6, 12])
+  expect(await program(Promise.resolve([1, 2, 3]))).toEqual([1, 2, 4, 2, 4, 8, 3, 6, 12])
+})
+
+it('should take the index as second argument', () => {
+  let program = pipe(
+    flatMap((_x: number, i) => i),
+    toArray()
+  )
+
+  expect(program([1, 2, 3])).toEqual([0, 1, 2])
+  expect(program([1, 2, 3])).toEqual([0, 1, 2])
+})
---
+++ src/flatMap.ts
@@ -0,0 +1,47 @@
+import { isAsyncIterable, isIterable } from './utils/iterator'
+import { LazyIterable } from './shared-types'
+
+type Fn<T, R> = (datum: T, index: number) => R
+
+export function flatMap<T, R>(fn: Fn<T, R>) {
+  return function flatMapFn(data: LazyIterable<T>) {
+    if (data == null) return
+
+    // Handle the async version
+    if (isAsyncIterable(data) || data instanceof Promise) {
+      return {
+        async *[Symbol.asyncIterator]() {
+          let stream = data instanceof Promise ? await data : data
+
+          let i = 0
+          for await (let datum of stream) {
+            let result = fn(datum, i++)
+            if (isAsyncIterable(result) || result instanceof Promise) {
+              let stream = result instanceof Promise ? await result : result
+              yield* stream
+            } else if (isIterable(result)) {
+              yield* result
+            } else {
+              yield result
+            }
+          }
+        },
+      }
+    }
+
+    // Handle the sync version
+    return {
+      *[Symbol.iterator]() {
+        let i = 0
+        for (let datum of data) {
+          let result = fn(datum, i++)
+          if (isIterable(result)) {
+            yield* result
+          } else {
+            yield result
+          }
+        }
+      },
+    }
+  }
+}
