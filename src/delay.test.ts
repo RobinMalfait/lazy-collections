2a8ca203675532148c6b00fe3674f8b45c044fb7
Robin Malfait <malfait.robin@gmail.com>
2022-08-14 16:01:47 +0200

    improve delay tests a bit to make it less brittle

--- src/delay.test.ts
+++ src/delay.test.ts
@@ -4,33 +4,35 @@ import { delay } from './delay'
 import { map } from './map'
 import { every } from './every'
 import { tap } from './tap'
-import { chunk } from './chunk'
 
-it('should delay each value by 100ms', async () => {
+it('should delay each value by 50ms', async () => {
+  let DELAY = 50
   let counter = jest.fn()
+
+  let first: number | null = null
 
   let program = pipe(
     // Create a range of 6 values.
     range(0, 5),
 
     // Delay each value with 100ms.
-    delay(100),
+    delay(DELAY),
 
     // Map each value to the current date.
-    map(() => Date.now()),
+    map(() => {
+      if (first === null) {
+        first = Date.now()
+      }
+
+      return Date.now() - first
+    }),
 
     // Call the counter, just to be sure that we actually saw 6 values.
     tap(() => counter()),
 
-    // Group values per 2, this way we can compare the difference between them.
-    chunk(2),
-
-    // Map the chunked values to the difference between the two values.
-    map(([a, z]: [number, number]) => z - a),
-
-    // Verify that the diff is 100ms or more, this way we can validate that
-    // there has been a delay of 100ms at minimum.
-    every((diff: number) => diff >= 100)
+    // Ensure that each item took at least DELAY ms to be processed, and can't exceed DELAY * 2.
+    // This also ensures that they are truly handled sequentially.
+    every((current, i) => current >= DELAY * i && current <= DELAY * (i + 1))
   )
 
   let result = await program()
