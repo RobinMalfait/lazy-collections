adab01c34c6b8774ba4a95166819f034415f49bf
Alex Vipond <alex.vipond@betterhelp.com>
2023-02-21 12:08:51 -0600

    add, test, and document `at`

---
+++ src/at.ts
@@ -0,0 +1,33 @@
+import { find } from './find'
+import { isAsyncIterable } from './utils/iterator'
+import { pipe } from './pipe'
+import { toArray } from './toArray'
+import { LazyIterable } from './shared-types'
+
+export function at<T>(index: number) {
+  if (index >= 0) {
+    return function atFn(data: LazyIterable<T>) {
+      return find((_, i) => (i === index))(data)
+    }
+  }
+  
+  /**
+   * To support counting back with a negative index, the whole iteraror has to be
+   * converted to an array. Then, `Array.prototype.at` can be used. This is slow,
+   * but it helps deliver a consistent UX.
+   */
+  return function atFn(data: LazyIterable<T>) {
+    if (isAsyncIterable(data) || data instanceof Promise) {
+      return (async () => {
+        let stream = data instanceof Promise ? await data : data
+
+        let program = pipe(toArray())
+        let array = await program(stream)
+
+        return array.at(index)
+      })()
+    }
+
+    return Array.from(data).at(index)
+  }
+}
