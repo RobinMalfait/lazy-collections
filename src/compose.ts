433f4bc78358bc68ace659fc990ea09280372d31
Alex Vipond <hello@alexvipond.dev>
2023-04-21 03:49:17 -0500

    Ensure that the `types` are generated properly (#32)
    * restore .d.ts build output
    
    * infer type of `f`
    
    * update changelog
    
    ---------
    
    Co-authored-by: Robin Malfait <malfait.robin@gmail.com>

--- src/compose.ts
+++ src/compose.ts
@@ -4,7 +4,7 @@ import { LazyIterable } from './shared-types'
 type Fn = (...args: any) => any
 
 export function compose<T>(fn: Fn | LazyIterable<T>, ...fns: (Fn | LazyIterable<T>)[]): Fn {
-  return fns.reduce((f: Fn, g) => {
+  return (fns as Fn[]).reduce((f, g) => {
     let g_ = ensureFunction(g)
     return (...args) => f(g_(...args))
   }, ensureFunction(fn))
--- src/pipe.ts
+++ src/pipe.ts
@@ -4,7 +4,7 @@ import { LazyIterable } from './shared-types'
 type Fn = (...args: any) => any
 
 export function pipe<T>(...fns: (Fn | LazyIterable<T>)[]): Fn {
-  return fns.reduceRight((f: Fn, g) => {
+  return (fns as Fn[]).reduceRight((f, g) => {
     let g_ = ensureFunction(g)
     return (...args) => f(g_(...args))
   }, ensureFunction(fns.pop()))
