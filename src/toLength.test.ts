1ac51f3a89e784193c0b8b03a8ec5f9cbae87b48
Alex Vipond <hello@alexvipond.dev>
2023-08-25 16:51:49 -0500

    add, test, and document toLength (#26)

---
+++ src/toLength.test.ts
@@ -0,0 +1,32 @@
+import { pipe } from './pipe'
+import { range } from './range'
+import { toLength } from './toLength'
+import { delay } from './delay'
+
+it('should return the length of the iterable', () => {
+  let program = pipe(
+    toLength()
+  )
+
+  expect(program(range(0, 25))).toBe(26)
+  expect(program(range(0, 25))).toBe(26)
+})
+
+it('should return the length of the iterable (async)', async () => {
+  let program = pipe(
+    delay(0),
+    toLength()
+  )
+  
+  expect(await program(range(0, 25))).toBe(26)
+  expect(await program(range(0, 25))).toBe(26)
+})
+
+it('should return the length of the iterable (Promise async)', async () => {
+  let program = pipe(
+    toLength()
+  )
+
+  expect(await program(Promise.resolve(range(0, 25)))).toBe(26)
+  expect(await program(Promise.resolve(range(0, 25)))).toBe(26)
+})
