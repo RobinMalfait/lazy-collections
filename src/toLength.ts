8e66ca72cda0351015b053a3417898e532cc3565
Robin Malfait <malfait.robin@gmail.com>
2023-09-09 00:50:45 +0200

    simplify `toLength`

--- src/toLength.ts
+++ src/toLength.ts
@@ -1,21 +1,5 @@
-import { isAsyncIterable } from './utils/iterator'
-import { pipe } from './pipe'
-import { toArray } from './toArray'
-import { LazyIterable } from './shared-types'
+import { reduce } from './reduce'
 
 export function toLength() {
-  return function toLengthFn(data: LazyIterable<any>) {
-    if (isAsyncIterable(data) || data instanceof Promise) {
-      return (async () => {
-        let stream = data instanceof Promise ? await data : data
-
-        let program = pipe(toArray())
-        let array = await program(stream)
-
-        return array.length
-      })()
-    }
-
-    return Array.from(data).length
-  }
+  return reduce((_value, acc) => acc + 1, 0)
 }
