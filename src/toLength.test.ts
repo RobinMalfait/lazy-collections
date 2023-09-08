c43882700099b701d90c6a38ed5461ae9a41ff1d
Robin Malfait <malfait.robin@gmail.com>
2023-09-09 00:45:36 +0200

    format tests

--- src/toLength.test.ts
+++ src/toLength.test.ts
@@ -4,28 +4,21 @@ import { toLength } from './toLength'
 import { delay } from './delay'
 
 it('should return the length of the iterable', () => {
-  let program = pipe(
-    toLength()
-  )
+  let program = pipe(toLength())
 
   expect(program(range(0, 25))).toBe(26)
   expect(program(range(0, 25))).toBe(26)
 })
 
 it('should return the length of the iterable (async)', async () => {
-  let program = pipe(
-    delay(0),
-    toLength()
-  )
-  
+  let program = pipe(delay(0), toLength())
+
   expect(await program(range(0, 25))).toBe(26)
   expect(await program(range(0, 25))).toBe(26)
 })
 
 it('should return the length of the iterable (Promise async)', async () => {
-  let program = pipe(
-    toLength()
-  )
+  let program = pipe(toLength())
 
   expect(await program(Promise.resolve(range(0, 25)))).toBe(26)
   expect(await program(Promise.resolve(range(0, 25)))).toBe(26)
